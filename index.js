3712b35cb13f956df38f6e21f3cf591eb28256d3
Muhammad Mustadi <muhammad.mustadi@gmail.com>
2017-02-16 00:01:50 +0700

    refactor on image

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const Bot = require('node-line-messaging-api');
 const axios = require('axios');
 const memecanvas = require('memecanvas');
-memecanvas.init('./images', '-meme');
+const fs = require('fs');
+const gcloud = require('gcloud');
+const uuid = require('uuid');
+
+const db = require('./database')
+
 const ID = `1501426113`
 const SECRET = `f5494121240b9b28455326b598d136cb`
 const TOKEN = `Q8D1JSQxJzrO7dtU+JuJbebMVFx14wNVh+3ragq6uHdZaHMjO/4ekz6/0e/Ii30ZHvyplC9GVeaKFQP5pebnT5I5R6eCFNFDRFfnbhfmTUE7SZFSFFH2SwTk5C8N0KF7cTavOxvfBpWPWQGeudSzXgdB04t89/1O/w1cDnyilFU=`
-var fs = require('fs');
-var gcloud = require('gcloud');
-var uuid = require('uuid');
 
 var config = {
   apiKey: "AIzaSyA0TWPApytG050nP7bRFcvbab7o5xFSRwI",
@@ -17,8 +19,6 @@ var config = {
   messagingSenderId: "1006578959505"
 };
 
-// Get a reference to the storage service, which is used to create references in your storage bucket
-// Create a root reference
 const gcs = gcloud.storage({
     projectId: 'memeline-76501',
     keyFilename: './memeline.json'
@@ -28,49 +28,136 @@ const bucket = gcs.bucket(`memeline-76501.appspot.com`);
 
 const PORT = process.env.PORT || 3002
 
-// console.log(new Bot.default())
-let bot = new Bot(SECRET, TOKEN, { webhook: { port: PORT, ngrok: false } })
-//
-// // bot webhook succesfully started
+const bot = new Bot(SECRET, TOKEN, { webhook: { port: PORT, ngrok: false } })
+
+const appRef = db.ref(ID)
+const eventsRef = appRef.child('events')
+const usersRef = appRef.child('users')
+const statusRef = appRef.child('status')
+
+memecanvas.init('./images', '-meme');
+
 bot.on('webhook', w => {
   console.log(`bot listens on port ${w}.`)
-  const ts = new Date()
-  axios.get('https://ipinfo.io').then(({data}) => data).then(console.log).catch(console.error)
+  axios.get('https://ipinfo.io').then(({data}) => statusRef.push({ data })).then(console.log).catch(console.error)
 })
 
-bot.on('text', (e) => {
-  let msgs = new Bot.Messages();
-  msgs.addText('Selamat Datang di MemeLine!').addText({text: 'Cara gampang bikin meme (>.<)'})
-  bot.replyMessage(e.replyToken, msgs.commit());
+bot.on('events', (e, req) => {
+  Promise.all(e.map(evt => eventsRef.push(evt))).then(() => {
+    console.log('pushed events')
+  })
+})
+
+bot.on('follow', ({replyToken, source}) => {
+  bot.getProfile(source[`${source.type}Id`]).then(({data: {displayName}}) => {
+    console.log(displayName)
+    bot.replyMessage(replyToken, new Messages().addText(`Selamat Datang di MemeLine, ${displayName}!`).addText({text: 'Cara gampang bikin meme (>.<)'}).commit())
+  })
+})
+
+bot.on('text', ({replyToken, source}) => {
+  bot.replyMessage(replyToken, new Bot.Messages().addText('harambae').commit())
 });
 
 bot.on('image', (e) => {
   let msgs = new Bot.Messages();
-  var randomName = uuid.v4();
+  const randomName = uuid.v4()
+  const { message: { id } } = e
+  bot.getContent(id)
+    .then(({data}) => {
+      const filePath = `./buffer/${randomName}.jpg`
+      return new Promise((resolve, reject) => {
+        fs.writeFile(filePath, new Buffer(data, 'binary'), 'binary', (err) => {
+          if (err) reject(err)
+          resolve(filePath)
+        })
+      })
+    })
+    .then(filePath => {
+      return createMeme({picture: filePath, top: 'TOP MEME', bottom: 'AWESOME'})
+    })
+    .then(replyImage => {
+      msgs.addText('RESULT').addImage(replyImage)
+      return bot.replyMessage(e.replyToken, msgs.commit())
+    })
+    .catch(err => console.error(err))
+  // var randomName = uuid.v4();
+  // bot.getContent(e.message.id).then(response => {
+  //   fs.writeFile(`./buffer/${randomName}.jpg`, new Buffer(response.data, 'binary'), 'binary', (err) => {
+  //     if (err) throw err;
+  //       memecanvas.generate(`./buffer/${randomName}.jpg`, 'Top of Meme', 'Bottom of Meme', (error, memefilename) => {
+  //         if (error){
+  //             console.log(error);
+  //         } else {
+  //             console.log(memefilename);
+  //             bucket.upload(memefilename, (err, file) => {
+  //                 if (err) { return console.error(err); }
+  //                 let publicUrl = `https://firebasestorage.googleapis.com/v0/b/${'memeline-76501'}.appspot.com/o/${file.metadata.name}?alt=media`;
+  //                 msgs.addText('HASIL:');
+  //                 msgs.addImage({originalUrl: publicUrl, previewUrl: publicUrl});
+  //                 bot.replyMessage(e.replyToken, msgs.commit());
+  //                 try {
+  //                   fs.unlink(memefilename);
+  //                   fs.unlink(`./buffer/${randomName}.jpg`);
+  //                 } catch(e) {
+  //                   console.log('ERROR DELETING FILE', e);
+  //                 }
+  //             });
+  //         }
+  //       });
+  //   });
+  // });
+});
 
-  bot.getContent(e.message.id).then(response => {
-    fs.writeFile(`./buffer/${randomName}.jpg`, new Buffer(response.data, 'binary'), 'binary', (err) => {
-      if (err) throw err;
-        memecanvas.generate(`./buffer/${randomName}.jpg`, 'Top of Meme', 'Bottom of Meme', (error, memefilename) => {
-          if (error){
-              console.log(error);
-          } else {
-              console.log(memefilename);
-              bucket.upload(memefilename, (err, file) => {
-                  if (err) { return console.error(err); }
-                  let publicUrl = `https://firebasestorage.googleapis.com/v0/b/${'memeline-76501'}.appspot.com/o/${file.metadata.name}?alt=media`;
-                  msgs.addText('HASIL:');
-                  msgs.addImage({originalUrl: publicUrl, previewUrl: publicUrl});
-                  bot.replyMessage(e.replyToken, msgs.commit());
-                  try {
-                    fs.unlink(memefilename);
-                    fs.unlink(`./buffer/${randomName}.jpg`);
-                  } catch(e) {
-                    console.log('ERROR DELETING FILE', e);
-                  }
-              });
+const getMemeByUser = (sourceId) => {
+  const currentMemeRef = usersRef.ref(`${sourceId}/currentMeme`)
+  return currentMemeRef.once('value')
+    .then((snapshot) => {
+      const currentMeme = snapshot.val()
+      const newMeme = {picture: null, top: null, bottom: null}
+      if (!currentMeme) {
+        return currentMemeRef.push(newMeme).then(() => Promise.reject(newMeme))
+      } else {
+        return Promise.resolve(currentMeme)
+      }
+    })
+}
+
+const createMeme = ({ picture, top, bottom }) => {
+  const randomName = uuid.v4();
+  return new Promise((resolve, reject) => {
+    memecanvas.generate(picture, top, bottom, (
+      error,
+      memefilename
+    ) => {
+      if (error) {
+        console.log(error);
+        reject(error)
+      } else {
+        console.log(memefilename);
+        bucket.upload(memefilename, (err, file) => {
+          if (err) {
+            console.error(err)
+            return reject(err);
+          }
+          let publicUrl = `https://firebasestorage.googleapis.com/v0/b/${'memeline-76501'}.appspot.com/o/${file.metadata.name}?alt=media`;
+          return resolve({originalUrl: publicUrl, previewUrl: publicUrl})
+          // msgs.addText('HASIL:');
+          // msgs.addImage({ originalUrl: publicUrl, previewUrl: publicUrl });
+          // bot.replyMessage(e.replyToken, msgs.commit());
+          try {
+            fs.unlink(memefilename);
+            fs.unlink(`./buffer/${randomName}.jpg`);
+          } catch (e) {
+            console.log('ERROR DELETING FILE', e);
           }
         });
+      }
     });
-  });
-});
+  })
+};
+
+
+// const initMeme = ({user}) => {
+//   return
+// }
